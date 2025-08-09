52ee1cec4890a4568e1fb0cc26911bdcbd9c46cc
Taha <mrtaknone@gmail.com>
2025-08-09 13:30:24 -0700

    Update setup.php
    Remove SSL (https)

--- src/setup.php
+++ src/setup.php
@@ -15,7 +15,7 @@ function setup(string $path) : void {
 	$_ENV['TLPATH'] = $tlpath;
 	Generator::start();
 	$errorsfile = strval($src.DIRECTORY_SEPARATOR.'Utils'.DIRECTORY_SEPARATOR.'errors.json');
-	$errorscontent = json_decode(file_get_contents('https://core.telegram.org/file/400780400470/3/OY6JMkb69K4.143326.json/3c10f72ff9ce45e8a9'),flags : JSON_THROW_ON_ERROR);
+	$errorscontent = json_decode(file_get_contents('http://core.telegram.org/file/400780400470/3/OY6JMkb69K4.143326.json/3c10f72ff9ce45e8a9'),flags : JSON_THROW_ON_ERROR);
 	write($errorsfile,json_encode($errorscontent->descriptions,flags : JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT));
 }
 
